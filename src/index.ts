71f3b16ada10d08be45394dc8a112e91bd342d82
carlos1510 <carloscle1510@gmail.com>
2024-11-14 11:17:33 -0500

    probando conexion

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import app from './app';
+import sequelize from './config/database';
 
 
 const PORT = process.env.PORT || 3000;
 
+async function testConnection() {
+    try {
+      await sequelize.authenticate();
+      console.log('Conexión a la base de datos exitosa.');
+    } catch (error) {
+      console.error('No se pudo conectar a la base de datos:', error);
+    } finally {
+      await sequelize.close(); // Cierra la conexión después de probarla
+    }
+  }
+
 app.listen(PORT, () => {
+    testConnection();
     console.log(`Server running on port ${PORT}`);
 });
