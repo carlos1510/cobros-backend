5fc20d0cb956019f0bef807b572cc8767f6781c0
carlos1510 <carloscle1510@gmail.com>
2024-12-26 11:38:06 -0500

    Agregar funcionalidad en servicios

--- src/routes/serviceRoutes.ts
+++ src/routes/serviceRoutes.ts
@@ -3,7 +3,7 @@ import ServiceController from '../controllers/ServiceController';
 
 const router = Router();
 
-router.get('/', ServiceController.index);
+router.get('/:userId', ServiceController.index);
 router.post('/save', ServiceController.store);
 router.put('/update/:id', ServiceController.update);
 router.delete('/destroy/:id', ServiceController.destroy);
